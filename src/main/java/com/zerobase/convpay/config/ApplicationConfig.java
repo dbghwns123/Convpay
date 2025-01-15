bd9b85f46b48197d2ab11b6a05a2270cc2ad8506
Ho-JunYu <hojoon1011@gmail.com>
2025-01-16 03:32:14 +0900

    JavaConfig ComponeneScan을 통한 빈 등록

--- src/main/java/com/zerobase/convpay/config/ApplicationConfig.java
+++ src/main/java/com/zerobase/convpay/config/ApplicationConfig.java
@@ -1,35 +1,39 @@
 package com.zerobase.convpay.config;
 
+import com.zerobase.convpay.ConvpayApplication;
 import com.zerobase.convpay.service.*;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
 import java.util.HashSet;
 
 @Configuration
+@ComponentScan(basePackages = "com.zerobase.convpay")
+//@ComponentScan(basePackageClasses = ConvpayApplication.class)
 public class ApplicationConfig {
 
-    @Bean
-    public ConveniencePayService conveniencePayService() {
-        return new ConveniencePayService(
-                new HashSet<>(Arrays.asList(moneyAdapter(), cardAdapter())), discountByConvenience());
-    }
-
-    @Bean
-    public static CardAdapter cardAdapter() {
-        return new CardAdapter();
-    }
-
-    @Bean
-    public static MoneyAdapter moneyAdapter() {
-        return new MoneyAdapter();
-    }
-
-    @Bean
-    public static DiscountByConvenience discountByConvenience() {
-        return new DiscountByConvenience();
-    }
+//    @Bean
+//    public ConveniencePayService conveniencePayService() {
+//        return new ConveniencePayService(
+//                new HashSet<>(Arrays.asList(moneyAdapter(), cardAdapter())), discountByConvenience());
+//    }
+//
+//    @Bean
+//    public static CardAdapter cardAdapter() {
+//        return new CardAdapter();
+//    }
+//
+//    @Bean
+//    public static MoneyAdapter moneyAdapter() {
+//        return new MoneyAdapter();
+//    }
+//
+//    @Bean
+//    public static DiscountByConvenience discountByConvenience() {
+//        return new DiscountByConvenience();
+//    }
 
 //    @Bean
 //    public ConveniencePayService conveniencePayServiceDiscountPayMethod() {
