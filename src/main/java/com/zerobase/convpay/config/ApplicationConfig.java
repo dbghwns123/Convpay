5e69c62f6935d175c69c7ed4eb2898fcbc49e94e
Ho-JunYu <hojoon1011@gmail.com>
2025-01-16 02:11:38 +0900

    스프링 프레임워크로 전환하기

--- src/main/java/com/zerobase/convpay/config/ApplicationConfig.java
+++ src/main/java/com/zerobase/convpay/config/ApplicationConfig.java
@@ -1,20 +1,40 @@
 package com.zerobase.convpay.config;
 
 import com.zerobase.convpay.service.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
 import java.util.HashSet;
 
+@Configuration
 public class ApplicationConfig {
 
-    public ConveniencePayService conveniencePayServiceDiscountConvenience() {
+    @Bean
+    public ConveniencePayService conveniencePayService() {
         return new ConveniencePayService(
-                new HashSet<>(Arrays.asList(new MoneyAdapter(), new CardAdapter())), new DiscountByConvenience());
+                new HashSet<>(Arrays.asList(moneyAdapter(), cardAdapter())), discountByConvenience());
     }
 
-    public ConveniencePayService conveniencePayServiceDiscountPayMethod() {
-        return new ConveniencePayService(
-                new HashSet<>(Arrays.asList(new MoneyAdapter(), new CardAdapter())), new DiscountByPayMethod());
+    @Bean
+    public static CardAdapter cardAdapter() {
+        return new CardAdapter();
     }
+
+    @Bean
+    public static MoneyAdapter moneyAdapter() {
+        return new MoneyAdapter();
+    }
+
+    @Bean
+    public static DiscountByConvenience discountByConvenience() {
+        return new DiscountByConvenience();
+    }
+
+//    @Bean
+//    public ConveniencePayService conveniencePayServiceDiscountPayMethod() {
+//        return new ConveniencePayService(
+//                new HashSet<>(Arrays.asList(new MoneyAdapter(), new CardAdapter())), new DiscountByPayMethod());
+//    }
 
 }
